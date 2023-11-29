ed4daf7ab2e3f545d1a5ea9ac1c290f07e9c6d58
Santripas <81268287+santripas@users.noreply.github.com>
2023-11-28 21:26:19 -0300

    continuação dos estudos

--- exemplo/src/App.js
+++ exemplo/src/App.js
@@ -1,11 +1,57 @@
+import React from 'react';
+import {createStore} from 'redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Olá da Danki Code</h2>
-    </div>
-  );
+const reducer = function(state,action){
+  if(action.type === "INC"){
+    return state + action.payload;
+  }else if(action.type === 'DESC'){
+    return state - action.payload;
+  }
+  return state;
+}
+
+const store = createStore(reducer,0);
+
+store.subscribe(function(){
+  console.log("O estado mundou!",store.getState());
+})
+
+store.dispatch({type:'INC',payload:3});
+store.dispatch({type:'INC',payload:3});
+store.dispatch({type:'INC',payload:3});
+store.dispatch({type:'DESC',payload:3});
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {count:0};
+    this.increment = this.increment.bind(this);
+  }
+
+  increment = ()=> {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+  
+  decrement = ()=> {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+  
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.decrement}>Menos!</button>
+        <button onClick={this.increment}>Mais!</button>
+      <h2>{this.state.count}</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
